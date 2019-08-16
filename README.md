aa49af9661415b2f16a7c72ca914e339ea721b7b
yennanliu <f339339@gmail.com>
2019-08-16 18:47:42 +0800

    fix to-save file directory and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NYC_Taxi_Pipeline
+
+
+### Process 
+```
+                             --> Stream pre-process --> stream dataset -->  stream pipeline --> real-time report 
+NYC tlc-trip-record-data ---> 
+
+							 --> Storage to DB/csv --> batch pipeline --> historical report  
+
+```
--- downloadData.py
+++ downloadData.py
@@ -12,7 +12,8 @@ def pre_steup():
 def prepare_data_url(start_month,end_month):
     urls=[]
     months = pd.date_range(start_month, end_month, freq='MS').strftime("%Y-%m").tolist()
-    travels = ['yellow', 'green']
+    #travels = ['yellow', 'green']
+    travels = ['yellow']
     for month in months:
         for travel in travels:
             url='https://s3.amazonaws.com/nyc-tlc/trip+data/{}_tripdata_{}.csv'.format(travel, month)
@@ -24,9 +25,12 @@ def Download_2_csv(data_urls):
     for data_url in data_urls:
         print ('>>> downloading ', data_url)
         file_name = data_url.split('/')[-1]
-        download = requests.get(data_url)
+        download = requests.get('/data'+ data_url)
+        count = 0 
         with open(file_name, 'w') as temp_file:
-            temp_file.writelines(download.content)
+            while count < 100:
+                temp_file.writelines(str(download.content))
+                count += 1 
 
 if __name__ == '__main__':
     pre_steup()
