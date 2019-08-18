2a3be7eee8ac2b003f966f5d450470caa877d3e4
yennanliu <f339339@gmail.com>
2019-08-18 14:14:44 +0800

    add sql set up db table

---
+++ batch/create_table.sql
@@ -0,0 +1,27 @@
+-- Create db in mysql
+CREATE DATABASE IF NOT EXISTS taxi;
+
+-- Create a table with yellow taxi trip 
+use taxi; 
+CREATE TABLE yellow_trip  (
+        id INT NOT NULL AUTO_INCREMENT,
+        vendor_name CHAR(10),
+        Trip_Pickup_DateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        Trip_Dropoff_DateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        Passenger_Count INTEGER NULL,
+        Trip_Distance FLOAT NOT NULL,
+        Start_Lon FLOAT NULL,
+        Start_Lat FLOAT NOT NULL,
+        Rate_Code CHAR(10) NULL,
+        store_and_forward CHAR(10) NULL,
+        End_Lon FLOAT NOT NULL,
+        End_Lat FLOAT NOT NULL,
+        Payment_Type CHAR(10) NOT NULL,
+        Fare_Amt FLOAT NOT NULL,
+        surcharge FLOAT NOT NULL,
+        mta_tax FLOAT NULL,
+        Tip_Amt FLOAT NULL,
+        Tolls_Amt FLOAT NULL,
+        Total_Amt FLOAT NULL,
+        PRIMARY KEY (id)
+        );
