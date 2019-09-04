af494017b30d2a8f4144b1f0e24920e83aff8612
yennanliu <f339339@gmail.com>
2019-09-05 00:54:37 +0800

    add func get distance within pickup and dropoff lon/lat

--- script/rdd_transform.py
+++ script/rdd_transform.py
@@ -1,33 +1,51 @@
 #################################################################
-# SCRIPT HELP GET TRANSFORMED RDD AND STATISTICS    
+# SCRIPT GET TRANSFORMED RDD AND STATISTICS    
 #################################################################
 
 class RDD_Transform(object):
 
     def __init__(self):
+
         self.rdd = sc.textfile("data/yellow_tripdata_sample.csv")
 
     def load_rdd(self, filename):
+
         if not self.rdd:
             self.rdd = sc.textfile(filename)
 
     def get_max_Total_Amt(self, rdd):
+        
         max_Total_Amt = self.rdd.map( lambda x : [x[2], x[19]]) \
                            .max(key = lambda x : x[2])
         return max_Total_Amt
 
     def get_group_max_Total_Amt(self, rdd):
+
+        def add(x,y):
+            return x + y 
+
         filter_rdd = rdd.map(lambda x : (float(x[19]), x[2]))
         grouped_sum = filter_rdd.map(lambda x: ((x[1]), x[0])).reduceByKey(add).collect()
         grouped_max = filter_rdd.map(lambda x: ((x[1]), x[0])).reduceByKey(max).collect()
         return grouped_max, grouped_sum 
 
-    def to_timestamp(self, x):
-        return datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-
     def get_timestamp_rdd(self, rdd):
+
+        def to_timestamp(x):
+            return datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
         filter_rdd = rdd.map(lambda x: x[3])
-        return filter_rdd.map(self.to_timestamp).collect()
+
+        return filter_rdd.map(to_timestamp).collect()
+
+    def get_distance_rdd(self, rdd):
+
+        def get_pseudo_distance(geo_array):
+            x1,y1,x2,y2 = float(geo_array[0]), float(geo_array[1]), float(geo_array[2]), float(geo_array[3])
+            return ((x1-x2)**2 + (y1-y2)**2)**(0.5)
+
+        return rdd.map(lambda x : [x[6],x[7], x[10],x[11] ]) \
+          .map(get_pseudo_distance).collect()
+
 
 
 # def get_max_Total_Amt(df):
@@ -44,8 +62,8 @@ class RDD_Transform(object):
 #                    Total_Amt = x['Total_Amt'] ))
 #     .max(key = lambda x : x['Total_Amt']))
 #     return max_Total_Amt
-
-
+#
+#
 # def get_group_max_Total_Amt(rdd):
 #     """
 #     input  : yellow taxi RDD
@@ -55,10 +73,10 @@ class RDD_Transform(object):
 #     grouped_sum = filter_rdd.map(lambda x: ((x[1]), x[0])).reduceByKey(add).collect()
 #     grouped_max = filter_rdd.map(lambda x: ((x[1]), x[0])).reduceByKey(max).collect()
 #     return grouped_max, grouped_sum 
-
+#
 # def to_timestamp(x):
 #     return datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-
+#
 # def get_timestamp_rdd(rdd):
 #     filter_rdd = rdd.map(lambda x: x[3])
 #     return filter_rdd.map(to_timestamp).collect()
