c613172c192d3a6b66d9bf2f6739ddc0e0dd947f
yennanliu <f339339@gmail.com>
2020-02-27 12:02:40 +0800

    fix to add main function make this script can be run via sbt (LoadReferenceData.scala)

--- src/main/scala/DataProcess/LoadReferenceData.scala
+++ src/main/scala/DataProcess/LoadReferenceData.scala
@@ -13,166 +13,167 @@ import java.util.Calendar
 
 object LoadReferenceData {
 
-    //Source, destination directories
-    val srcDataDirRoot = "/data/staging/reference-data/" 
-    val destDataDirRoot = "/data/processed/reference/" 
+    def main(args: Array[String]){ 
+        //Source, destination directories
+        val srcDataDirRoot = "data/staging/reference-data/" 
+        val destDataDirRoot = "data/processed/reference/" 
 
 
-    val sc = new SparkContext("local[*]", "LoadReferenceData")   
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val spark = SparkSession
-        .builder
-        .appName("LoadReferenceData")
-        .master("local[*]")
-        .config("spark.sql.warehouse.dir", "/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
-        .getOrCreate()
+        val sc = new SparkContext("local[*]", "LoadReferenceData")   
+        val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+        val spark = SparkSession
+            .builder
+            .appName("LoadReferenceData")
+            .master("local[*]")
+            .config("spark.sql.warehouse.dir", "/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
+            .getOrCreate()
 
-    //1.  Taxi zone lookup
-    val taxiZoneSchema = StructType(Array(
-        StructField("location_id", StringType, true),
-        StructField("borough", StringType, true),
-        StructField("zone", StringType, true),
-        StructField("service_zone", StringType, true)))
+        //1.  Taxi zone lookup
+        val taxiZoneSchema = StructType(Array(
+            StructField("location_id", StringType, true),
+            StructField("borough", StringType, true),
+            StructField("zone", StringType, true),
+            StructField("service_zone", StringType, true)))
 
-    //2. Months of the year
-    val tripMonthNameSchema = StructType(Array(
-        StructField("trip_month", StringType, true),
-        StructField("month_name_short", StringType, true),
-        StructField("month_name_full", StringType, true)))
+        //2. Months of the year
+        val tripMonthNameSchema = StructType(Array(
+            StructField("trip_month", StringType, true),
+            StructField("month_name_short", StringType, true),
+            StructField("month_name_full", StringType, true)))
 
-    //3.  Rate code id lookup
-    val rateCodeSchema = StructType(Array(
-        StructField("rate_code_id", IntegerType, true),
-        StructField("description", StringType, true)))
+        //3.  Rate code id lookup
+        val rateCodeSchema = StructType(Array(
+            StructField("rate_code_id", IntegerType, true),
+            StructField("description", StringType, true)))
 
-    //4.  Payment type lookup
-    val paymentTypeSchema = StructType(Array(
-        StructField("payment_type", IntegerType, true),
-        StructField("abbreviation", StringType, true),
-        StructField("description", StringType, true)))
+        //4.  Payment type lookup
+        val paymentTypeSchema = StructType(Array(
+            StructField("payment_type", IntegerType, true),
+            StructField("abbreviation", StringType, true),
+            StructField("description", StringType, true)))
 
-    //5. Trip type
-    val tripTypeSchema = StructType(Array(
-        StructField("trip_type", IntegerType, true),
-        StructField("description", StringType, true)))
-
-
-    //6. Vendor ID
-    val vendorSchema = StructType(Array(
-        StructField("vendor_id", IntegerType, true),
-        StructField("abbreviation", StringType, true),
-        StructField("description", StringType, true)))
+        //5. Trip type
+        val tripTypeSchema = StructType(Array(
+            StructField("trip_type", IntegerType, true),
+            StructField("description", StringType, true)))
 
 
-    def loadReferenceData(srcDatasetName: String, srcDataFile: String, destDataDir: String, srcSchema: StructType, delimiter: String )
-    {
-      println("Dataset:  " + srcDatasetName)
-      println(".......................................................") 
-      
-      //Execute for idempotent runs
-      //println("....deleting destination directory - " + dbutils.fs.rm(destDataDir, recurse=true))
-      
-      //Read source data
-      val refDF = spark.read.option("header", "true")
-                          .schema(srcSchema)
-                          .option("delimiter",delimiter)
-                          .csv(srcDataFile)
+        //6. Vendor ID
+        val vendorSchema = StructType(Array(
+            StructField("vendor_id", IntegerType, true),
+            StructField("abbreviation", StringType, true),
+            StructField("description", StringType, true)))
+
+
+        def loadReferenceData(srcDatasetName: String, srcDataFile: String, destDataDir: String, srcSchema: StructType, delimiter: String )
+        {
+          println("Dataset:  " + srcDatasetName)
+          println(".......................................................") 
           
-      //Write parquet output
-      println("....reading source and saving as parquet")
-      refDF.coalesce(1).write.parquet(destDataDir)
-      
-      //Delete residual files from job operation (_SUCCESS, _start*, _committed*)
-      println("....deleting flag files")
-      //dbutils.fs.ls(destDataDir + "/").foreach((i: FileInfo) => if (!(i.path contains "parquet")) dbutils.fs.rm(i.path))
-      
-      println("....done")
+          //Execute for idempotent runs
+          //println("....deleting destination directory - " + dbutils.fs.rm(destDataDir, recurse=true))
+          
+          //Read source data
+          val refDF = spark.read.option("header", "true")
+                              .schema(srcSchema)
+                              .option("delimiter",delimiter)
+                              .csv(srcDataFile)
+              
+          //Write parquet output
+          println("....reading source and saving as parquet")
+          refDF.coalesce(1).write.parquet(destDataDir)
+          
+          //Delete residual files from job operation (_SUCCESS, _start*, _committed*)
+          println("....deleting flag files")
+          //dbutils.fs.ls(destDataDir + "/").foreach((i: FileInfo) => if (!(i.path contains "parquet")) dbutils.fs.rm(i.path))
+          
+          println("....done")
+        }
+
+        loadReferenceData("taxi zone",srcDataDirRoot + "taxi_zone_lookup.csv",destDataDirRoot + "taxi-zone",taxiZoneSchema,",")
+        loadReferenceData("trip month",srcDataDirRoot + "trip_month_lookup.csv",destDataDirRoot + "trip-month",tripMonthNameSchema,",")
+        loadReferenceData("rate code",srcDataDirRoot + "rate_code_lookup.csv",destDataDirRoot + "rate-code",rateCodeSchema,"|")
+        loadReferenceData("payment type",srcDataDirRoot + "payment_type_lookup.csv",destDataDirRoot + "payment-type",paymentTypeSchema,"|")
+        loadReferenceData("trip type",srcDataDirRoot + "trip_type_lookup.csv",destDataDirRoot + "trip-type",tripTypeSchema,"|")
+        loadReferenceData("vendor",srcDataDirRoot + "vendor_lookup.csv",destDataDirRoot + "vendor",vendorSchema,"|")
+
+        //display(dbutils.fs.ls("/mnt/workshop/curated/nyctaxi/reference/"))
+
+        // COMMAND ----------
+
+        // MAGIC %md
+        // MAGIC ### 5. Create Hive tables
+
+        // COMMAND ----------
+
+        // MAGIC %sql 
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS taxi_zone_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS taxi_zone_lookup(
+        // MAGIC location_id STRING,
+        // MAGIC borough STRING,
+        // MAGIC zone STRING,
+        // MAGIC service_zone STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/taxi-zone/';
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS trip_month_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS trip_month_lookup(
+        // MAGIC trip_month STRING,
+        // MAGIC month_name_short STRING,
+        // MAGIC month_name_full STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/trip-month/';
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS rate_code_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS rate_code_lookup(
+        // MAGIC rate_code_id INT,
+        // MAGIC description STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/rate-code/';
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS payment_type_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS payment_type_lookup(
+        // MAGIC payment_type INT,
+        // MAGIC abbreviation STRING,
+        // MAGIC description STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/payment-type/';
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS trip_type_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS trip_type_lookup(
+        // MAGIC trip_type INT,
+        // MAGIC description STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/trip-type/';
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC DROP TABLE IF EXISTS vendor_lookup;
+        // MAGIC CREATE TABLE IF NOT EXISTS vendor_lookup(
+        // MAGIC vendor_id INT,
+        // MAGIC abbreviation STRING,
+        // MAGIC description STRING)
+        // MAGIC USING parquet
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/vendor/';
     }
-
-    loadReferenceData("taxi zone",srcDataDirRoot + "taxi_zone_lookup.csv",destDataDirRoot + "taxi-zone",taxiZoneSchema,",")
-    loadReferenceData("trip month",srcDataDirRoot + "trip_month_lookup.csv",destDataDirRoot + "trip-month",tripMonthNameSchema,",")
-    loadReferenceData("rate code",srcDataDirRoot + "rate_code_lookup.csv",destDataDirRoot + "rate-code",rateCodeSchema,"|")
-    loadReferenceData("payment type",srcDataDirRoot + "payment_type_lookup.csv",destDataDirRoot + "payment-type",paymentTypeSchema,"|")
-    loadReferenceData("trip type",srcDataDirRoot + "trip_type_lookup.csv",destDataDirRoot + "trip-type",tripTypeSchema,"|")
-    loadReferenceData("vendor",srcDataDirRoot + "vendor_lookup.csv",destDataDirRoot + "vendor",vendorSchema,"|")
-
-    //display(dbutils.fs.ls("/mnt/workshop/curated/nyctaxi/reference/"))
-
-    // COMMAND ----------
-
-    // MAGIC %md
-    // MAGIC ### 5. Create Hive tables
-
-    // COMMAND ----------
-
-    // MAGIC %sql 
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS taxi_zone_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS taxi_zone_lookup(
-    // MAGIC location_id STRING,
-    // MAGIC borough STRING,
-    // MAGIC zone STRING,
-    // MAGIC service_zone STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/taxi-zone/';
-
-    // COMMAND ----------
-
-    // MAGIC %sql
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS trip_month_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS trip_month_lookup(
-    // MAGIC trip_month STRING,
-    // MAGIC month_name_short STRING,
-    // MAGIC month_name_full STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/trip-month/';
-
-    // COMMAND ----------
-
-    // MAGIC %sql
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS rate_code_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS rate_code_lookup(
-    // MAGIC rate_code_id INT,
-    // MAGIC description STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/rate-code/';
-
-    // COMMAND ----------
-
-    // MAGIC %sql
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS payment_type_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS payment_type_lookup(
-    // MAGIC payment_type INT,
-    // MAGIC abbreviation STRING,
-    // MAGIC description STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/payment-type/';
-
-    // COMMAND ----------
-
-    // MAGIC %sql
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS trip_type_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS trip_type_lookup(
-    // MAGIC trip_type INT,
-    // MAGIC description STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/trip-type/';
-
-    // COMMAND ----------
-
-    // MAGIC %sql
-    // MAGIC use taxi_db;
-    // MAGIC DROP TABLE IF EXISTS vendor_lookup;
-    // MAGIC CREATE TABLE IF NOT EXISTS vendor_lookup(
-    // MAGIC vendor_id INT,
-    // MAGIC abbreviation STRING,
-    // MAGIC description STRING)
-    // MAGIC USING parquet
-    // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/reference/vendor/';
-
 
 } 
