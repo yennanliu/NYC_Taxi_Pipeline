50f1a1feaf680131718c7ac896099ac859bcb833
yennanliu <f339339@gmail.com>
2020-03-02 12:20:30 +0800

    fix sql, save directory make script TransformGreenTaxiData.scala work

--- src/main/scala/DataTransform/TransformGreenTaxiData.scala
+++ src/main/scala/DataTransform/TransformGreenTaxiData.scala
@@ -16,127 +16,139 @@ import java.util.Calendar
 
 object TransformGreenTaxiData { 
 
-    val sc = new SparkContext("local[*]", "TransformGreenTaxiData")   
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val spark = SparkSession
-        .builder
-        .appName("TransformGreenTaxiData")
-        .master("local[*]")
-        .config("spark.sql.warehouse.dir", "/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
-        .getOrCreate()
+    def main(args: Array[String]){ 
 
-  //Destination directory
-  val destDataDirRoot =  "/output/transactions/green-taxi" 
+        val sc = new SparkContext("local[*]", "TransformGreenTaxiData")   
+        val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+        val spark = SparkSession
+            .builder
+            .appName("TransformGreenTaxiData")
+            .master("local[*]")
+            .config("spark.sql.warehouse.dir", "/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
+            .getOrCreate()
 
-  val curatedDF = spark.sql("""
-    select 
-        t.taxi_type,
-        t.vendor_id,
-        t.pickup_datetime,
-        t.dropoff_datetime,
-        t.store_and_fwd_flag,
-        t.rate_code_id,
-        t.pickup_location_id,
-        t.dropoff_location_id,
-        t.pickup_longitude,
-        t.pickup_latitude,
-        t.dropoff_longitude,
-        t.dropoff_latitude,
-        t.passenger_count,
-        t.trip_distance,
-        t.fare_amount,
-        t.extra,
-        t.mta_tax,
-        t.tip_amount,
-        t.tolls_amount,
-        t.ehail_fee,
-        t.improvement_surcharge,
-        t.total_amount,
-        t.payment_type,
-        t.trip_type,
-        t.trip_year,
-        t.trip_month,
-        v.abbreviation as vendor_abbreviation,
-        v.description as vendor_description,
-        tt.description as trip_type_description,
-        tm.month_name_short,
-        tm.month_name_full,
-        pt.description as payment_type_description,
-        rc.description as rate_code_description,
-        tzpu.borough as pickup_borough,
-        tzpu.zone as pickup_zone,
-        tzpu.service_zone as pickup_service_zone,
-        tzdo.borough as dropoff_borough,
-        tzdo.zone as dropoff_zone,
-        tzdo.service_zone as dropoff_service_zone,
-        year(t.pickup_datetime) as pickup_year,
-        month(t.pickup_datetime) as pickup_month,
-        day(t.pickup_datetime) as pickup_day,
-        hour(t.pickup_datetime) as pickup_hour,
-        minute(t.pickup_datetime) as pickup_minute,
-        second(t.pickup_datetime) as pickup_second,
-        year(t.dropoff_datetime) as dropoff_year,
-        month(t.dropoff_datetime) as dropoff_month,
-        day(t.dropoff_datetime) as dropoff_day,
-        hour(t.dropoff_datetime) as dropoff_hour,
-        minute(t.dropoff_datetime) as dropoff_minute,
-        second(t.dropoff_datetime) as dropoff_second
-    from 
-      taxi_db.green_taxi_trips_raw t
-      left outer join taxi_db.vendor_lookup v 
-        on (t.vendor_id = v.vendor_id)
-      left outer join taxi_db.trip_type_lookup tt 
-        on (t.trip_type = tt.trip_type)
-      left outer join taxi_db.trip_month_lookup tm 
-        on (t.trip_month = tm.trip_month)
-      left outer join taxi_db.payment_type_lookup pt 
-        on (t.payment_type = pt.payment_type)
-      left outer join taxi_db.rate_code_lookup rc 
-        on (t.rate_code_id = rc.rate_code_id)
-      left outer join taxi_db.taxi_zone_lookup tzpu 
-        on (t.pickup_location_id = tzpu.location_id)
-      left join taxi_db.taxi_zone_lookup tzdo 
-        on (t.dropoff_location_id = tzdo.location_id)
-    """)
+        import spark.implicits._
 
-  //Save as Delta, partition by year and month
-  curatedDF
-    .coalesce(3)
-    .write
-    .format("delta")
-    .mode("append")
-    .partitionBy("trip_year","trip_month")
-    .save(destDataDirRoot)   
+        //Destination directory
 
-  // COMMAND ----------
+        val destDataDirRoot =  "data/output/transactions/green-taxi"
+        val srcDataFile = "data/processed"
 
-  //Cluster conf: 3 autoscale to 6 workers - DS4v2 (with DS13v2 driver) - 8 cores, 28 GB of RAM/worker | Yellow + green together with 128 MB raw delta files | 3.5 minutes
+        // load processed data
 
-  // COMMAND ----------
+        val vendor_lookup = spark.read
+                            .option("header","true")
+                            .option("delimiter", ",")
+                            .csv(srcDataFile + "/reference/vendor/" + "*.csv")
 
-  // MAGIC %md
-  // MAGIC ### 3.  Define external table
+        val trip_type = spark.read
+                        .option("header","true")
+                        .option("delimiter", ",")
+                        .csv(srcDataFile + "/reference/trip-type/" + "*.csv")
 
-  // COMMAND ----------
+        val trip_month = spark.read
+                         .option("header","true")
+                         .option("delimiter", ",")
+                         .csv(srcDataFile + "/reference/trip-month/" + "*.csv")
 
-  // MAGIC %sql
-  // MAGIC use taxi_db;
-  // MAGIC 
-  // MAGIC DROP TABLE IF EXISTS green_taxi_trips_curated;
-  // MAGIC CREATE TABLE green_taxi_trips_curated
-  // MAGIC USING DELTA
-  // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/transactions/green-taxi';
+        val trip_zone = spark.read
+                        .option("header","true")
+                        .option("delimiter", ",")
+                        .csv(srcDataFile + "/reference/taxi-zone/" + "*.csv")
 
-  // COMMAND ----------
+        val rate_code = spark.read
+                        .option("header","true")
+                        .option("delimiter", ",")
+                        .csv(srcDataFile + "/reference/rate-code/" + "*.csv")
 
-  // MAGIC %sql
-  // MAGIC select trip_year,trip_month, count(*) as trip_count from taxi_db.green_taxi_trips_curated group by trip_year,trip_month
-  // MAGIC order by trip_year, trip_month
 
-  // COMMAND ----------
+        val payment_type = spark.read
+                           .option("header","true")
+                           .option("delimiter", ",")
+                           .csv(srcDataFile + "/reference/payment-type/" + "*.csv")
 
-  // MAGIC %sql
-  // MAGIC select count(*) from taxi_db.green_taxi_trips_curated
+        val green_taxi = spark.read
+                          .option("header","true")
+                          .option("delimiter", ",")
+                          .csv(srcDataFile + "/green-taxi/*/*/" + "*.csv")
 
+        // RDD -> spark sql table
+
+        vendor_lookup.createOrReplaceTempView("vendor_lookup")
+        trip_type.createOrReplaceTempView("trip_type")
+        trip_month.createOrReplaceTempView("trip_month")
+        trip_zone.createOrReplaceTempView("trip_zone")
+        rate_code.createOrReplaceTempView("rate_code")
+        payment_type.createOrReplaceTempView("payment_type")
+        green_taxi.createOrReplaceTempView("green_taxi")
+
+        // merge spark sql table
+        
+        val curatedDF = spark.sql("""
+          WITH CTE AS
+            (SELECT green_taxi.*,
+                    year(pickup_datetime) AS pickup_year,
+                    month(pickup_datetime) AS pickup_month,
+                    day(pickup_datetime) AS pickup_day,
+                    hour(pickup_datetime) AS pickup_hour,
+                    minute(pickup_datetime) AS pickup_minute,
+                    second(pickup_datetime) AS pickup_second,
+                    date(pickup_datetime) AS pickup_date,
+                    year(dropoff_datetime) AS dropoff_year,
+                    month(dropoff_datetime) AS dropoff_month,
+                    day(dropoff_datetime) AS dropoff_day,
+                    hour(dropoff_datetime) AS dropoff_hour,
+                    minute(dropoff_datetime) AS dropoff_minute,
+                    second(dropoff_datetime) AS dropoff_second,
+                    date(dropoff_datetime) AS dropoff_date
+                    FROM green_taxi)
+          SELECT DISTINCT t.*,
+                          v.abbreviation AS vendor_abbreviation,
+                          v.description AS vendor_description,
+                          tm.month_name_short,
+                          tm.month_name_full,
+                          pt.description AS payment_type_description,
+                          rc.description AS rate_code_description,
+                          tzpu.borough AS pickup_borough,
+                          tzpu.zone AS pickup_zone,
+                          tzpu.service_zone AS pickup_service_zone,
+                          tzdo.borough AS dropoff_borough,
+                          tzdo.zone AS dropoff_zone,
+                          tzdo.service_zone AS dropoff_service_zone
+                          FROM CTE t                        
+                          LEFT OUTER JOIN vendor_lookup v ON (t.vendor_id = v.vendor_id)
+                          LEFT OUTER JOIN trip_month tm ON (t.pickup_month = tm.trip_month)
+                          LEFT OUTER JOIN payment_type pt ON (t.payment_type = pt.payment_type)
+                          LEFT OUTER JOIN rate_code rc ON (t.rate_code_id = rc.rate_code_id)
+                          LEFT OUTER JOIN trip_zone tzpu ON (t.pickup_location_id = tzpu.location_id)
+                          LEFT OUTER JOIN trip_zone tzdo ON (t.dropoff_location_id = tzdo.location_id)
+
+              """)
+
+      curatedDF.show()
+
+      //Save as csv, partition by year and month
+      curatedDF
+          .repartition(1)  //save output in 1 csv by month by year, can do the "larger" repartition when work on the whole dataset
+          .write
+          .format("csv")
+          .mode("append")
+          .option("header","true")
+          .partitionBy("pickup_year","pickup_month")
+          .save(destDataDirRoot)   
+
+        // COMMAND ----------
+
+        // MAGIC %sql
+        // MAGIC use taxi_db;
+        // MAGIC 
+        // MAGIC DROP TABLE IF EXISTS green_taxi_trips_curated;
+        // MAGIC CREATE TABLE green_taxi_trips_curated
+        // MAGIC USING DELTA
+        // MAGIC LOCATION '/mnt/workshop/curated/nyctaxi/transactions/green-taxi';
+
+        // COMMAND ----------
+
+  }
 
 }
