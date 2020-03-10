f858c6af1aae2ef5ad6cb11dfbb59a2ce17e36df
yennanliu <f339339@gmail.com>
2020-03-10 10:18:11 +0800

    add utils class, and config/aws_creds.txt.dev

---
+++ src/main/scala/utils/SetUpAWS.scala
@@ -0,0 +1,18 @@
+package utils 
+
+object SetUpAWS{
+def setupAwsCreds() = {
+    import scala.io.Source
+
+    for (line <- Source.fromFile("config/aws_creds.txt.dev").getLines) {
+      val fields = line.split(" ")
+      //println (line)
+      println ( fields(0) + "=" + fields(1))
+      if (fields.length == 2) {
+
+        System.setProperty( fields(0), fields(1) )
+      }
+    }
+  }
+
+}
