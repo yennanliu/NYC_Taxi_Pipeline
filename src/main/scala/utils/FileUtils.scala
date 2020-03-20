da1e309e7980b77e8d900fcfe77e1056bbc4ff48
yennanliu <f339339@gmail.com>
2020-03-20 10:45:49 +0800

    add utils FileUtils.scala

---
+++ src/main/scala/utils/FileUtils.scala
@@ -0,0 +1,38 @@
+package utils
+
+import java.io.{BufferedReader, File, FileNotFoundException, InputStreamReader}
+import java.util.stream.Collectors
+
+import org.apache.commons.io.FilenameUtils
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.SparkSession
+import org.json4s.DefaultFormats
+
+import scala.io.Source
+import scala.collection.JavaConverters._
+
+object FileUtils {
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else if (d.isFile) {
+      List(d)
+    } else {
+      throw new FileNotFoundException(s"No Files to Run ${dir}")
+    }
+  }
+
+  def getContentFromFileAsString(file: File): String = {
+    scala.io.Source.fromFile(file).mkString //    //By scala.io. on read spark fail with legit error when path does not exists
+  }
+
+  def readFileWithHadoop(path: String, sparkSession: SparkSession): String = {
+    val file = new Path(path)
+    val hadoopConf = sparkSession.sessionState.newHadoopConf()
+    val fs = file.getFileSystem(hadoopConf)
+    val fsFile = fs.open(file)
+    val reader = new BufferedReader(new InputStreamReader(fsFile))
+    reader.lines.collect(Collectors.joining)
+  }
+}
