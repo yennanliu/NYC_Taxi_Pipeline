713baa0cd7991fe0192352686205bf68fb0b7092
yennanliu <f339339@gmail.com>
2020-03-27 11:23:18 +0800

    fix var name, evernt from (for kibana visualize) (CreateTaxiOrderGeoEvent.scala), clean no need log data

--- src/main/scala/TaxiEvent/CreateTaxiOrderGeoEvent.scala
+++ src/main/scala/TaxiEvent/CreateTaxiOrderGeoEvent.scala
@@ -30,19 +30,25 @@ object CreateTaxiOrderGeoEvent {
             val id_driver = r.nextInt(10)
             val id_passenger = r.nextInt(100)
             val event_date = System.currentTimeMillis
-            val long = 40.71 + (r.nextFloat - 0.5) * 0.001 // generate random long 
+            val lon = 40.71 + (r.nextFloat - 0.5) * 0.001 // generate random long 
             var lat = 74.00 + (r.nextFloat - 0.5) * 0.001 // generate random long 
 
+            // val payload =
+            //   s"""
+            //      |{ "id": $id,
+            //      |  "event_date": $event_date,
+            //      |  "tour_value": $tour_value,
+            //      |  "id_driver": $id_driver,
+            //      |  "id_passenger": $id_passenger,
+            //      |  "long" : $long,
+            //      |  "lat" : $lat}
+            //   """.stripMargin
+
+
             val payload =
-              s"""
-                 |{ "id": $id,
-                 |  "event_date": $event_date,
-                 |  "tour_value": $tour_value,
-                 |  "id_driver": $id_driver,
-                 |  "id_passenger": $id_passenger,
-                 |  "long" : $long,
-                 |  "lat" : $lat}
-              """.stripMargin
+               s"""
+                { "id": $id, "event_date": $event_date, "tour_value": $tour_value, "id_driver": $id_driver, "id_passenger": $id_passenger, "location" : [$lat, $lon]}
+                """.stripMargin
 
             Thread.sleep(sleepDelayMs)
             out.write(payload)
