0027ad2c667b4940b18c93abee4f0fcaa909b918
yennanliu <f339339@gmail.com>
2020-05-31 11:35:17 +0800

    add resources/application.conf, /LoadCreds.scala, /GetCreds.scala

---
+++ src/main/scala/DevClass/LoadCreds.scala
@@ -0,0 +1,20 @@
+package DevClass
+
+import java.util.concurrent.TimeUnit
+import java.io.File
+
+// user defined class 
+import utils.GetCreds
+
+object LoadCreds {
+
+  def main(args: Array[String]): Unit = {
+
+    println(">>> import user creds")
+
+    GetCreds
+    
+
+  }
+
+}
---
+++ src/main/scala/utils/GetCreds.scala
@@ -0,0 +1,32 @@
+package utils 
+
+import com.typesafe.config.ConfigFactory
+
+object GetCreds{
+
+      // mongo 
+      val mongodbconfig = ConfigFactory.load().getConfig("mongodb")
+
+      val connection_string:String = mongodbconfig.getString("CONNECTION_STRING")
+      val database_name:String = mongodbconfig.getString("DATABASE")
+      val collection_name:String = mongodbconfig.getString("COLLECTION")
+
+      // kafka 
+      val kafkaconfig = ConfigFactory.load().getConfig("kafka")
+      val bootstrap_servers:String = kafkaconfig.getString("BOOTSTRAP_SERVERS")
+      val topic:String = kafkaconfig.getString("TOPIC")
+
+      // spark 
+      val sparkconfig = ConfigFactory.load().getConfig("spark")
+      val master_url:String = sparkconfig.getString("MASTER_URL")
+      
+      println (connection_string)
+      println (database_name)
+      println (collection_name)
+
+      println (bootstrap_servers)
+      println (topic)
+
+      println (master_url)
+
+}
